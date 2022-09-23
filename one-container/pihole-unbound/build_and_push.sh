72c5a9f64f87fc31acff640ed2afa793eb7add67
Dick Pluim <67138922+pluim003@users.noreply.github.com>
2022-09-23 13:42:57 +0200

    First 64bit is being created and then the 32bit

--- one-container/pihole-unbound/build_and_push.sh
+++ one-container/pihole-unbound/build_and_push.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
 PIHOLE_VER=`cat VERSION`
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7 -t pluim003/pihole-unbound:$PIHOLE_VER-arm32v7 --push .
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7 -t pluim003/pihole-unbound:latest-arm32v7 --push .
 
 docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm64/v8 -t pluim003/pihole-unbound:$PIHOLE_VER-arm64v8 --push .
 docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm64/v8 -t pluim003/pihole-unbound:latest-arm64v8 --push .
 
+docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7 -t pluim003/pihole-unbound:$PIHOLE_VER-arm32v7 --push .
+docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm/v7 -t pluim003/pihole-unbound:latest-arm32v7 --push .
+
 docker manifest  rm pluim003/pihole-unbound:latest  pluim003/pihole-unbound:latest-arm64v8 pluim003/pihole-unbound:latest-arm32v7 pluim003/pihole-unbound:$PIHOLE_VER
 docker manifest create  pluim003/pihole-unbound:latest -a pluim003/pihole-unbound:latest-arm64v8 -a pluim003/pihole-unbound:latest-arm32v7
-docker manifest create  pluim003/pihole-unbound:$PIHOLE_VER -a pluim003/pihole-unbound:latest-arm64v8 -a pluim003/pihole-unbound:latest-arm32v7
+docker manifest create  pluim003/pihole-unbound:$PIHOLE_VER -a pluim003/pihole-unbound:$PIHOLE_VER-arm64v8 -a pluim003/pihole-unbound:$PIHOLE_VER-arm32v7
 
 docker manifest inspect pluim003/pihole-unbound:latest
 docker manifest push pluim003/pihole-unbound:latest
 docker manifest push pluim003/pihole-unbound:$PIHOLE_VER
-
-
-
