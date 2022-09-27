92ecfa59be55e00c250bc3b6651ce6222c9e5aac
pluim003 <dick@dickpluim.com>
2022-09-27 18:19:00 +0200

    Dockerfile and script for nightly build

---
+++ one-container/pihole-unbound/build_and_push_nightly.sh
@@ -0,0 +1,6 @@
+# Run this once: docker buildx create --use --name build --node build --driver-opt network=host
+PIHOLE_VER=nightly
+
+docker buildx build --no-cache --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm64/v8,linux/arm/v7,linux/amd64 -f Dockerfile_nightly -t pluim003/pihole-unbound:nightly --push .
+
+
