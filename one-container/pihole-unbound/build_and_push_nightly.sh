87d9b8b3e243dc4e5b4296ef448f111e7f930860
pluim003 <67138922+pluim003@users.noreply.github.com>
2022-09-30 22:33:02 +0200

    Added creation of arm/v6-image

--- one-container/pihole-unbound/build_and_push_nightly.sh
+++ one-container/pihole-unbound/build_and_push_nightly.sh
@@ -2,5 +2,5 @@
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
 PIHOLE_VER=nightly
 
-docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm64/v8,linux/arm/v7,linux/amd64 -f Dockerfile_nightly -t pluim003/pihole-unbound:nightly --push .
+docker buildx build --build-arg PIHOLE_VERSION=$PIHOLE_VER --platform linux/arm64/v8,linux/arm/v7,linux/amd64,linux/arm/v6 -f Dockerfile_nightly -t pluim003/pihole-unbound:nightly --push .
 
