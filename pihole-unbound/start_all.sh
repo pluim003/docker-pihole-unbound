180c2b49ac4ef14049a82d7791f3bbc0bf176f05
Dick Pluim <67138922+pluim003@users.noreply.github.com>
2023-11-18 12:40:49 +0100

    Update start_all.sh

--- pihole-unbound/start_all.sh
+++ pihole-unbound/start_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 /usr/sbin/unbound-anchor -a /var/lib/unbound/root.key
 chown unbound:unbound /var/lib/unbound/root.key
-/usr/bin/start.sh
 touch /run/openrc/softlevel
 rc-update add unbound default
 rc-service unbound start
+/usr/bin/start.sh
